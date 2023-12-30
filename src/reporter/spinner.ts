1b3656744a46ec050b5d98fe30f580e8d14e9adf
be5invis <belleve@typeof.net>
2023-12-30 04:38:06 -0800

    Simplify spinner code

--- src/reporter/spinner.ts
+++ src/reporter/spinner.ts
@@ -9,8 +9,9 @@ const isLegacyConsole =
 
 const ansiRegex = /(\u009B|\u001B\[)[0-?]*[ -\/]*[@-~]/g;
 
-const spinnerInterval = 100;
-const spinnerFrames = [
+const SpinnerFastInterval = 10; // 100 fps
+const SpinnerSlowInterval = 50; // 20 fps
+const SpinnerFrames = [
 	"⢀⠀",
 	"⡀⠀",
 	"⠄⠀",
@@ -75,23 +76,31 @@ export interface SpinnerTextSource {
 	getText(): string;
 }
 
+enum SpinnerState {
+	Stopped = 0, // Spinner is stopped. Nothing to happen.
+	Starting = 1, // Spinner is starting. It will show the message at next frame.
+	Running = 2, // Spinner is running.
+	Paused = 3, // Spinner is paused.
+}
+
 export default class Spinner {
 	textSource: SpinnerTextSource | null = null;
 	private lastTextSize: number = 0;
 	private frame: number = 0;
-	private frames: string[] = isLegacyConsole ? spinnerFramesCP437 : spinnerFrames;
-	private interval: number = spinnerInterval;
-	private enabled: boolean = true;
-	private currentFrameWritten: boolean = false;
+	private frames: string[] = isLegacyConsole ? spinnerFramesCP437 : SpinnerFrames;
+	private interval: number = SpinnerFastInterval;
 
-	private id: null | NodeJS.Timer = null;
+	private state: SpinnerState = SpinnerState.Stopped;
+	private timer: null | NodeJS.Timer = null;
+	private currentFrameWritten: boolean = false;
 
 	private stream = process.stderr;
 	private outputIsTty =
 		process.stdout.isTTY && process.stderr.isTTY && !(process.env.TERM === "dumb");
 
-	clear() {
-		if (!this.enabled || !this.outputIsTty) return;
+	private clear() {
+		if (!this.outputIsTty) return;
+		cliCursor.show(this.stream);
 		readline.clearLine(this.stream, 0);
 		readline.cursorTo(this.stream, 0);
 		this.lastTextSize = 0;
@@ -101,9 +110,42 @@ export default class Spinner {
 		return this.textSource ? this.textSource.getText() : "";
 	}
 
-	nextFrame() {
-		if (!this.enabled || !this.outputIsTty) return;
+	private nextFrame() {
+		switch (this.state) {
+			case SpinnerState.Stopped:
+				return this.handleStopped();
+			case SpinnerState.Starting:
+				return this.handleStarting();
+			case SpinnerState.Paused:
+				return this.handlePaused();
+			case SpinnerState.Running:
+				return this.handleRunning();
+		}
+	}
+	private handleStopped() {
+		if (this.timer != null) {
+			clearInterval(this.timer);
+			this.timer = null;
+		}
+	}
+	private handleStarting() {
+		if (this.timer == null) {
+			this.interval = SpinnerFastInterval;
+			this.timer = setInterval(() => this.timerCallback(), this.interval);
+		}
 
+		this.state = SpinnerState.Running;
+	}
+	private handleRunning() {
+		if (this.timer === null || this.interval === SpinnerFastInterval) {
+			this.interval = SpinnerSlowInterval;
+			if (this.timer != null) clearInterval(this.timer);
+			this.timer = setInterval(() => this.timerCallback(), this.interval);
+		}
+
+		if (!this.outputIsTty) return;
+
+		cliCursor.hide(this.stream);
 		readline.cursorTo(this.stream, 0);
 		const text = this.getText();
 		const sizeText = this.textSize(text);
@@ -114,31 +156,36 @@ export default class Spinner {
 		if (!this.currentFrameWritten) this.frame = (this.frame + 1) % this.frames.length;
 		this.currentFrameWritten = true;
 	}
-
-	timerCallback() {
+	private handlePaused() {
+		if (this.timer != null) {
+			clearInterval(this.timer);
+			this.timer = null;
+		}
+	}
+	private timerCallback() {
 		this.currentFrameWritten = false;
 		this.nextFrame();
 	}
 
 	start() {
-		this.enabled = true;
-		if (this.id !== null) clearInterval(this.id);
+		if (this.state === SpinnerState.Running || this.state === SpinnerState.Starting) return;
+		this.state = SpinnerState.Starting;
 		this.nextFrame();
-		this.id = setInterval(() => this.timerCallback(), this.interval);
-		cliCursor.hide(this.stream);
 	}
 
 	pause() {
-		if (this.id !== null) clearInterval(this.id);
-		this.clear();
-		cliCursor.show(this.stream);
-		this.enabled = false;
+		if (this.state === SpinnerState.Stopped || this.state === SpinnerState.Paused) return;
+		if (this.state === SpinnerState.Running) this.clear();
+		this.state = SpinnerState.Paused;
+		this.nextFrame();
 	}
 
 	stop() {
-		this.pause();
+		if (this.state === SpinnerState.Stopped) return;
+		if (this.state === SpinnerState.Running) this.clear();
+		this.state = SpinnerState.Stopped;
+		this.nextFrame();
 		this.stream.write(chalk.cyan(this.getText()));
-		this.enabled = false;
 	}
 
 	private textSize(t: string) {
